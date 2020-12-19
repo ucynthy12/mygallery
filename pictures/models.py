6d11f71cdf08a720e2441d5912e7105b47522c93
cynthia umutoniwabo <ucynthy12@gmail.com>
2020-12-19 12:19:15 +0200

    created category,location and image class with methods

--- pictures/models.py
+++ pictures/models.py
@@ -1,6 +1,60 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+class Location(models.Model):
+    name= models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+        
+class Image(models.Model):
+    gallery_image = models.ImageField(upload_to = 'album/')
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True,blank=True,null=True)
+
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
+    
+    @classmethod
+    def update_image(cls,id,name):
+        cls.objects.filter(id=id).update(name=name)
+
+    @classmethod
+    def get_image_by_id(cls,id):
+      image=cls.objects.filter(id=id).all()
+      return image
+
+
+
+    @classmethod
+    def search_image(category):
+        album = cls.objects.filter(category__name__icontains=search_term)
+        return album
+
+    @classmethod
+    def filter_by_location(cls,location):
+        image_loc = Image.objects.filter(location_name=location).all()
+        return image_loc
+
+    def __str__(self):
+
+        return self.name
+
+    class Meta:
+        ordering = ['pub_date']
 
     
